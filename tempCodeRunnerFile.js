8d3b20b06ad6659b0828eb84e9aa11336fc15231
AvinashS97 <avikumar97170@gmail.com>
2024-02-04 14:15:19 +0530

    n

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log (x == 8); /* equal to */
+console.log(x!= 8);  /* not equal to */
+console.log(x > 8); /* greater than */
+console.log(x < 8);  /* less than */
+console.log(x >= 8);   /* greater than or equal to */ 
+console.log(x <= 8);  /* less
