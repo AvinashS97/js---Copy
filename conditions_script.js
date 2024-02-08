fe2f68d4010502513f02554aa26a889636423bb4
AvinashS97 <avikumar97170@gmail.com>
2024-02-08 16:17:10 +0530

    08Feb

--- conditions_script.js
+++ conditions_script.js
@@ -42,6 +42,41 @@ console.log(x !== 5);  /* not equal value and not equal type */
 console.log("This is Logical Operator" );
 a = 5;
 b= 8;
-console.log (a != b);
-console.log (a!=b) && (b!= a);
-console.log (a!=b) || (b== a);
+console.log (a != b);        /* logical not operator */
+console.log (a!=b) && (b!= a);  /* logical and operator */
+console.log (a!=b) || (b== a);  /* logical or operator*/
+
+
+// conditional statement (if,else-if, else)
+
+console.log("this is conditional section");
+let age=18;
+
+if(age == 0){
+    console.log("really!");
+}
+
+else if(age >= 18){
+    console.log("You can drive");
+}
+
+else{
+    console.log("You can't drive");
+}
+
+// Ternary Operator - it use  if- else condition in one line.
+
+/*
+    syntax- condition ? exp 1 : exp 2
+    (marks>10)? "exp 1":"exp 2" 
+*/
+let p = 6;
+let q = 8;
+// let c = p>q? (p-q): (q-p)
+// console.log("c");
+ if (p>q){
+    let c=p-q;
+ }
+ else{
+    let c=q-p;
+ }
