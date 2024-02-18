30a7dbd9b4bdf856db14edfce2a144369846c41b
AvinashS97 <avikumar97170@gmail.com>
2024-02-18 21:51:46 +0530

    18feb2.0

---
+++ hs4.js
@@ -0,0 +1,25 @@
+// Undefined, NUll, BigInt
+
+// Undefined
+let  FirstName ;
+console.log(typeof FirstName);
+FirstName = "AVinash";  /*we can assign value after declaration of variable */
+console.log(typeof FirstName, FirstName);
+
+var  FirstName2 ;
+console.log(typeof FirstName2);
+
+/* ( Const are not  allowed to be change) */
+// const  FirstName3 ;
+// console.log(typeof FirstName3);  /* It gives error*/
+
+// Null
+let myVariable= null;
+console.log(typeof myVariable); /* type of Null is Object but Null is primitive data-type */
+
+// BigInt
+let myNumber= BigInt(1234);
+let myNumber2= 12345n;
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(myNumber+ myNumber2); /*we can't perform any action on BigInt and Other types */
+console.log(myNumber2);
