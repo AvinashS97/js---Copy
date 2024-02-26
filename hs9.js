32732dd42ee6509f28d4a70395f96969dc82cac3
AvinashS97 <avikumar97170@gmail.com>
2024-02-26 20:46:24 +0530

    26Feb

--- hs9.js
+++ hs9.js
@@ -2,3 +2,24 @@
 // For Loop
 
 // While Loop
+// Ex---> print 1 to 10
+let i=0;
+while(i<=10){
+    console.log(i);
+    i++
+}
+
+// Ex---> Print the sum of N number
+// let N=0;
+// let total=0;
+// while(N<=10){
+//     total= total+N;
+//     N++;
+// }
+// console.log(total);
+
+// OR 
+// This method is fast as compare to above method.
+let num=10;
+let total= (num*(num+1))/2;
+console.log(total);
