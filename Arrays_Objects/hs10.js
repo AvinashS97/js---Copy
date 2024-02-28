f14ad80b994348a162c4ced658c2bfab52ad8f1e
AvinashS97 <avikumar97170@gmail.com>
2024-02-28 12:56:40 +0530

    28Feb2.0

--- Arrays_Objects/hs10.js
+++ Arrays_Objects/hs10.js
@@ -1,1 +1,16 @@
-// Arrays---> ordered collectons of items. 
+// Intro in Arrays 
+// Arrays ---> ordered collectons of items. Array is mutable.
+
+let fruits= ["apple", "Mango", "Grapes"];
+// console.log(fruits);
+// for printing of any index value
+// console.log(fruits[2]); 
+//for chenging th evalue of any element
+fruits[2]= "banana";
+console.log(fruits);
+// we can store any type of element in arrays.
+let mixed= [1,2,3.14, "Avinash", null, undefined]; 
+console.log(mixed);
+// Type of array is Object
+console.log(typeof fruits);
+console.log(Array.isArray(fruits)); // to check it is arrays or not
---
+++ Arrays_Objects/hs11.js
@@ -0,0 +1,13 @@
+// Arrays Methods
+//PUSH, POP, SHIFT, UNSHIFT
+
+//push methods ---> Add new element.
+let fruits=["apple", "Mango", "Grapes"];
+//fruits.push("banana"); 
+console.log(fruits);
+
+// pop Method ---> it will remove the last element
+// fruits.pop()
+// console.log(fruits);
+fruits.unshift("banana");
+console.log(fruits);
