a77f75f607bccf9574f8d066818c63f67bee3ea6
AvinashS97 <avikumar97170@gmail.com>
2024-03-03 23:00:23 +0530

    4March

---
+++ Arrays_Objects/hs12.js
@@ -0,0 +1,9 @@
+// Primitive vs reference data types
+// primitive
+let num1= 6;
+let num2 = num1;
+console.log("Value of num1 is", num1);
+console.log("value of num2 is", num2); 
+num1++;
+console.log("Value of num1 is", num1);
+console.log("value of num2 is", num2);
