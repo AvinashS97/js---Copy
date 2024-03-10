8fb7b69f50d4cfadc3d954478d9c984660f8e763
AvinashS97 <avikumar97170@gmail.com>
2024-03-10 22:17:53 +0530

    10.2

--- Arrays_Objects/hs13.js
+++ Arrays_Objects/hs13.js
@@ -1,11 +1,23 @@
 // for loop in array
+// while loop in array
+
+// for loop in array
 let fruits=["apple", "mango","grapes"];
 
 // console.log(fruits.length);
 // console.log(fruits[fruits.length-1]);
-let fruits2=[];
 
 for (let i=0; i<fruits.length; i++){
     console.log(fruits[i].toUpperCase()); 
 }
-console.log(fruits2);  
+
+// While loop in array
+console.log("This is swhile loop")
+const fruits1=["Apple", "Mango","Grapes"];
+let i=0;
+while(i<fruits1.length){
+    console.log(fruits1[i].toLowerCase());
+    i++;
+}
+
+// for of loop in array
