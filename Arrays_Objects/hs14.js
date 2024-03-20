df9736949f3e2e4d693f746fb3d6d350dbf70cdf
AvinashS97 <avikumar97170@gmail.com>
2024-03-20 17:28:14 +0530

    20March

--- Arrays_Objects/hs14.js
+++ Arrays_Objects/hs14.js
@@ -1,1 +1,13 @@
 // Array Destructuring
+
+
+const myArray=["value1","value2","value3","value4"];
+// let val1 = myArray[0];
+// let val2 = myArray[1];
+// console.log("value of val1 is", val1);
+// console.log("value of val2  is", val2);
+// Or
+let [val1, val2, ...myNewArray]= myArray;
+console.log("value of val1 is", val1);
+console.log("value of val2  is", val2);
+console.log(myNewArray);
