e3e9873749fa6342b9d5d752a5e199175e7b50c8
AvinashS97 <avikumar97170@gmail.com>
2024-03-21 13:48:57 +0530

    21March

--- Arrays_Objects/hs12.js
+++ Arrays_Objects/hs12.js
@@ -22,7 +22,9 @@ console.log("After update array2", array2);
 //  How to clone array
 let arr1 = ["item1", "item2"];  
 // let arr2= ["item1", "item2"];   //method 1---> put the same element 
+
 // let arr2=arr1.slice(0).concat(["item3"]); //method 2 ---> using slice method
+
 // let arr2=[].concat(arr1,["item3"]) ; // method3 ---> add the previous array with empty array
 // let arr2=[...arr1, "item3"]; //spread operator
 let arr3= ["item4", "item5"];
@@ -31,3 +33,14 @@ let arr2= [...arr1, ...arr3 ];
 console.log("arr1 is ",arr1);
 console.log("arr2 is ",arr2);
 console.log(arr1 === arr2 );
+
+const newArray= [1,2,3,[4,5,10],4,5,6,[7,8,[9,0]]];
+const Real_Array= newArray.flat(Infinity);
+console.log("Real_Array is",Real_Array);
+console.log(Array.isArray("Avinash"));
+console.log(Array.from("Avinash"));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+console.log(Array.of(score1,score2,score3));
--- Arrays_Objects/hs15.js
+++ Arrays_Objects/hs15.js
@@ -1,6 +1,9 @@
 //Objects ---> Objects store key value pairs, It is reerence type, Objects don't have index
 // How to create Objects
-const person={name:"Avinash",age:20};
+const person={
+    name:"Avinash",    
+    age:20,
+};
 console.log(person);
 console.log(person.name);//Access of data in Object
 person.SurName= "Sharma"; //Add new key value in Object
