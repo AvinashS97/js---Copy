17eebfa40728a654be22128f3e35c47e6249e7cc
AvinashS97 <avikumar97170@gmail.com>
2024-03-26 22:43:06 +0530

    26.2

--- Arrays_Objects/hs16.js
+++ Arrays_Objects/hs16.js
@@ -1,5 +1,7 @@
 // Iterate in Objects
 // Object.keys
+// Computed Properties
+//Spread Operator
 
 const person={
     name:"Avinash",    
@@ -14,6 +16,39 @@ const person={
 // }
 // console.log(Object.keys(person))
 
-for(let key of Object.keys(person)){
-    console.log( person[key]);
+// for(let key of Object.keys(person)){
+//     console.log( person[key]);
+// }
+
+// Computed Properties
+// const key1="objkey1";
+// const key2="objkey2";
+
+// const value1="myvalue1";
+// const value2="myvalue2";
+
+// const obj ={
+//     [key1]:value1,
+//     [key2]:value2,
+// }
+// console.log(obj);
+
+//Spread Operator in Array
+
+// const array1=[1,2,3]; //Ex. of spread Operator in Array
+// const array2= [5,6,7];
+// const Newarray3 = [...array1, ...array2,89, ..."abc"];
+// console.log(Newarray3);
+
+// Spread Operator in Object
+const obj1={
+    key1:"value1",
+    key2:"value2",
 }
+const obj2={
+    key1:"valueChange",
+    key3:"value3",
+    key4:"value4",
+};
+const newObject={...obj1, ...obj2, key10:"value10"}
+console.log(newObject)
