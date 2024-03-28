42d3462bcdc60a866f10c81659b196df691c16f2
AvinashS97 <avikumar97170@gmail.com>
2024-03-28 15:16:23 +0530

    28March

--- Arrays_Objects/hs16.js
+++ Arrays_Objects/hs16.js
@@ -2,6 +2,7 @@
 // Object.keys
 // Computed Properties
 //Spread Operator
+//Object destructuring
 
 const person={
     name:"Avinash",    
@@ -39,9 +40,9 @@ const person={
 // const array2= [5,6,7];
 // const Newarray3 = [...array1, ...array2,89, ..."abc"];
 // console.log(Newarray3);
-
+                     
 // Spread Operator in Object
-const obj1={
+const obj1={   
     key1:"value1",
     key2:"value2",
 }
@@ -50,5 +51,43 @@ const obj2={
     key3:"value3",
     key4:"value4",
 };
-const newObject={...obj1, ...obj2, key10:"value10"}
-console.log(newObject)
+// const newObject={...obj1, ...obj2, key10:"value10"};
+// const newObject= {..."abc"};
+// console.log(newObject)
+
+//Object destructuring ---> create a variable and store the values
+
+const band={
+    bandName:"led zeppelin",
+    famousSong:"stairway in heaven",
+    year:1968,
+};
+// const bandName= band.bandName;
+// const famousSong = band.famousSong;
+//OR
+// const {bandName, famousSong,...restprp}=band;
+// console.log(bandName);
+// console.log(famousSong);
+// console.log(restprp);
+
+// Objects inside Array ---> too store large number of values...
+
+const users =[
+    {userid: 1, firstname:"Avinash", gender: "male"}, 
+    {userid: 2, firstname:"Avv", gender: "male"}, 
+    {userid: 3, firstname:"Shh", gender: "Female"},
+];
+// console.log(users);
+// for(let user of users){
+//     console.log(user);
+    // console.log(user.userid);
+// }
+
+// Nested destructuring
+
+// const [user1, user2, user3]= users;
+// console.log(user1); 
+    
+const[{firstname}, ,{gender} ]= users;
+console.log(firstname);
+console.log(gender);
