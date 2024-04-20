ac7ed99e39b27353008923c504beb7a0fa6b90fa
AvinashS97 <avikumar97170@gmail.com>
2024-04-20 21:44:55 +0530

    20March

--- 3_Functions/hs17.js
+++ 3_Functions/hs17.js
@@ -1,13 +1,22 @@
 function myFunc(theObject) {
     theObject.make = "Toyota";
   }
-  
   const mycar = {
     make: "Honda",
     model: "Accord",
     year: 1998,
-  };
-  
+  };  
   console.log(mycar.make); // "Honda"
 //   myFunc(mycar);
   console.log(mycar.make); // "Toyota"
+
+
+function singsong(){
+  console.log("happy Birthday");
+}
+singsong();
+
+function sum(num1, num2){
+  return num1 + num2;
+}
+console.log(sum(4,5));
