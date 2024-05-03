9d26c244591933c194744facb2abbeba2f32f7cb
AvinashS97 <avikumar97170@gmail.com>
2024-05-03 22:08:19 +0530

    3May

--- 6_OOP_PrototypeInheritance/hs28.js
+++ 6_OOP_PrototypeInheritance/hs28.js
@@ -1,1 +1,35 @@
-//Methods
+//METHODS
+// FUNCTION INSIDE OBJECT
+
+// const person ={
+//     firstName: "Mohit",
+//     age: 18,
+//     about: function(){
+//         console.log(`person name is ${this.firstName} and age is ${this.age}`); 
+//         console.log(this);
+//     }
+// }
+// person.about()
+
+function info(){
+    console.log(`person name is ${this.firstName} and age is ${this.age}`);
+}
+const person1 ={
+    firstName: "Mohit",
+    age: 18,
+    about: info
+}
+const person2 ={
+    firstName: "Avinash",
+    age: 20,
+    about: info
+}
+const person3 ={
+    firstName: "Sumit",
+    age: 22,
+    about: info
+}
+console.log(info);
+person1.about();
+person2.about();
+person3.about();
