cdef09170eb809ba1e97ec884a6fe03034ae6c3c
AvinashS97 <avikumar97170@gmail.com>
2024-05-05 18:50:35 +0530

    May

--- 6_OOP_PrototypeInheritance/hs30.js
+++ 6_OOP_PrototypeInheritance/hs30.js
@@ -1,14 +1,37 @@
 //[[PROTOTYPE]] ---> __proto__
 
-const student=['avinash','mohit','sumit','Aman']
-console.log(student);
+const student={
+    id1:'avinash',
 
-function person(fname,lname){
-    this.firstname = fname
-    this.lastname =lname
 }
 
-person.prototype.gender = "male";
+const student2={
+    roll:11,
+    __proto__:student
+}
+// console.log(student);
+console.log(student2);
 
-const person1 = new person("avinash", "Kumar");
-console.log(person1);
+// function person(fname,lname){
+//     this.firstname = fname
+//     this.lastname =lname
+// }
+
+// person.prototype.gender = "male";
+
+// const person1 = new person("avinash", "Kumar");
+// const person2 = new person("Aman", "Singh");
+// console.log(person1);
+// console.log(person2);
+
+//Changing value 
+
+// function person(){
+//     this.name ="Elon Musk"
+// }
+// person.prototype.age=25;
+// const person1 = new person();
+// person.prototype={age:52};
+// const person2= new person()
+// console.log(person1.age);
+// console.log(person2.age);
---
+++ 6_OOP_PrototypeInheritance/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+//     this.name ="Elon Musk"
+// }
+// person.prototype.age=25;
+// const person1 = new person();
+// person.prototype={age:52};
+// const person2= new person()
+// console.log(person1.age);
+// console.log(person
