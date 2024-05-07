8b934bdcadbe8d51937950394913f7895fcbf110
AvinashS97 <avikumar97170@gmail.com>
2024-05-07 19:16:03 +0530

    07May

--- 6_OOP_PrototypeInheritance/hs31.js
+++ 6_OOP_PrototypeInheritance/hs31.js
@@ -1,1 +1,13 @@
-// NEW KEYWORD
+// NEW KEYWORD ---> It create empty object with this , return this and make chaining/ relatiion with fns.
+
+function cUser(firstname, age){
+    this.firstname = firstname;
+    this.age = age;
+}
+
+cUser.prototype.about =function(){ 
+    console.log(this.firstname, this.age);
+}
+
+const user1 = new cUser("Avi",9);
+user1.about();
