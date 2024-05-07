0d1c9b0e3d596e699c916a16143bd66641e3226c
AvinashS97 <avikumar97170@gmail.com>
2024-05-07 21:20:57 +0530

    07

--- 6_OOP_PrototypeInheritance/hs31.js
+++ 6_OOP_PrototypeInheritance/hs31.js
@@ -1,13 +1,15 @@
-// NEW KEYWORD ---> It create empty object with this , return this and make chaining/ relatiion with fns.
+// NEW KEYWORD ---> It create empty object with this , return this and it bind the prpt that is declared in thus keyword to the new object.
+// Syntax: constructorFunction(arguments);
 
 function cUser(firstname, age){
     this.firstname = firstname;
     this.age = age;
 }
-
-cUser.prototype.about =function(){ 
-    console.log(this.firstname, this.age);
+ 
+cUser.prototype.about =function(){
+    console.log(this.firstname, this.age)
 }
 
 const user1 = new cUser("Avi",9);
+console.log(user1) // 
 user1.about();
