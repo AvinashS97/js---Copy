0042ea64fb7ac71e82fbb95cd0779c4b9b96b5a9
AvinashS97 <avikumar97170@gmail.com>
2024-05-08 21:02:31 +0530

    Get&Set()

--- 6_OOP_PrototypeInheritance/hs31.js
+++ 6_OOP_PrototypeInheritance/hs31.js
@@ -1,16 +1,19 @@
+// NEW KEYWORD
+// GETTER AND SETTERS
+
 // NEW KEYWORD ---> It create empty object with this , return this and it bind the prpt that is declared in thus keyword to the new object.
 //  Syntax: constructorFunction(arguments);
 
-function cUser(firstname, age){
-    this.firstname = firstname;
-    this.age = age;
-}
+// function cUser(firstname, age){
+//     this.firstname = firstname;
+//     this.age = age;
+// }
  
-cUser.prototype.about =function(){
-    console.log(this.firstname, this.age)
-}
+// cUser.prototype.about =function(){
+//     console.log(this.firstname, this.age)
+// }
 
-const user1 = new cUser("Avi",9);
+// const user1 = new cUser("Avi",9);
 // console.log(user1) 
 // user1.about();
 
@@ -25,3 +28,19 @@ const fruit1 = new Fruit('Yellow', 'Sweet', 1);
  
 // Display the result
 console.log(fruit1.color);
+   
+// GETTER AND SETTERS --->  Getters and setters are defined using the get and set keywords respectively.
+
+class person{
+    constructor(firstN, lastN, age1){
+        this.firstN =firstN;
+        this.lastN = lastN;
+        this.age1 = age1;
+    }
+    get fullname(){
+        return `${this.firstN} ${this.lastN}`
+    }
+}
+const person1 = new person("Avinash", "Sharma", 9);
+console.log(person1);
+console.log(person1.fullname);
