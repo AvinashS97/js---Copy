58d841517f5bc9feac8dda84ffe4a0fcc2f5e811
AvinashS97 <avikumar97170@gmail.com>
2024-05-09 22:08:30 +0530

    static

--- 6_OOP_PrototypeInheritance/hs32.js
+++ 6_OOP_PrototypeInheritance/hs32.js
@@ -1,3 +1,16 @@
 //STATIC METHOD & PROPERTIES
 
 // “this” keyword is used to call a static method within any other static method in JavaScript.
+// static keyword is used before the function name
+
+class Person{
+    constructor(name,YOb,gender){
+        this.name = name;
+        this.Yob = Yob;
+        this.gender = gender;
+    }
+     static greet(){
+        console.log('Hey There! How are you?');
+     }
+}
+Person.greet();
