6d98b77152343eddd1684ec06bca2b6128a900ed
AvinashS97 <avikumar97170@gmail.com>
2024-05-12 17:37:10 +0530

    How JS Works

--- 7_HowJsWorks/hs33.js
+++ 7_HowJsWorks/hs33.js
@@ -1,3 +1,5 @@
 // How JS Works...
-// 1.compilation of code 
+// 1.compilation of code ---> tokenization-->Parsing--->Code generation
 // 2.execution of code
+//How JS Works
+// firstly code goes to than Execution context is created, Memory allocation for code and than code execution phase start.
