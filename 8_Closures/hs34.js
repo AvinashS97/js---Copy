3df480ee10a15793ecc083844caa4958478d0c99
AvinashS97 <avikumar97170@gmail.com>
2024-05-14 22:21:30 +0530

    closure

--- 8_Closures/hs34.js
+++ 8_Closures/hs34.js
@@ -1,1 +1,13 @@
 //CLOSURES ---> A closure gives you access to an outer function's scope from an inner function.
+//function can return functions
+
+function outerFunction(){
+    function innerFunction(){
+        console.log("Hello i'm in inner fns.")
+    }
+    return innerFunction;
+}
+
+const ans = outerFunction();
+// console.log(ans);
+ans();
