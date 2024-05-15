77dc2880030073869e8a18b3c0d1999eb8914226
AvinashS97 <avikumar97170@gmail.com>
2024-05-15 22:25:36 +0530

    15

--- 8_Closures/hs34.js
+++ 8_Closures/hs34.js
@@ -1,25 +1,32 @@
+//LEXICAL SCOPING
+//CLOSURES
+
+
+// lexical Scoping
+
+// function outer(){
+//     let name = "Avinash";
+//     function inner(){
+//         console.log(name);
+//     }
+//     inner(); // inner fns have access to the variable of outer fns.
+// }
+// outer();
+
+
 //CLOSURES ---> A closure is the combination of a fns bundled together.
 //closure give us access to an outer fns scope from an inner fns.
 //function can return functions
 
-// lexical Scoping
-
-function outer(){
-    let name = "Avinash";
-    function inner(){
-        console.log(name);
-    }
-    inner(); // inner fns have access to the variable of outer fns.
-}
-outer();
-
 function outerFunction(){
+    let name1 = "Avi";
     function innerFunction(){
-        console.log("Hello i'm in inner fns.")
+        // console.log("Hello i'm in inner fns.")
+        console.log(name1);
     }
-    return innerFunction;
+    return innerFunction;// it return whole lexical scope
 }
 
-const ans = outerFunction();
+const ans = outerFunction();// 
 // console.log(ans);
 ans();
