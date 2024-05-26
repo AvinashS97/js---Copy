2ffff0bbaa29d35f65f8e92512fd3ad68ecbe076
AvinashS97 <avikumar97170@gmail.com>
2024-05-26 22:22:30 +0530

    ff

--- 9_DOM/hs35.js
+++ 9_DOM/hs35.js
@@ -1,2 +1,7 @@
 //DOM ---> Documnet Object Module
 //THe HTML DOM is a standard for how to get, change,add, or delete HTML elements.
+// When a web page is loaded, the browser creates a Document Object Model (DOM) of the page.
+// HTML ko JavaScript ke andr access krne ka tarika DOM kehlata hai.
+
+console.dir(document.body);
+console.log(document.body);
