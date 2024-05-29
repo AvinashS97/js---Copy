bb2d49f628014f9481d78419e66347eedf632783
AvinashS97 <avikumar97170@gmail.com>
2024-05-29 22:36:11 +0530

    Dom Selectors

--- 9_DOM/hs35.js
+++ 9_DOM/hs35.js
@@ -20,17 +20,40 @@
 // console.dir(document.getElementById("main-heading")); // it return objects.
 
 //select element using query selector
-// console.log(document.querySelector("#main-heading")); // it return objects. 
+// console.log(document.querySelector("#main-heading")); // it return objects.
+document.querySelector("h1"); //<h1 class=​"logo">​DOM Website​</h1>​
+document.querySelector("input[type=password]"); //document.querySelector("input[type="password"]")
+
+const list=document.querySelectorAll("li")
+
+list[0].style.color = "green"
+
+list.forEach(function(l){
+    l.style.backgroundColor = "red"
+})
 
 //Change Text (DOM MANIPULATION)
 // const mainHeading = document.getElementById("main-heading");
 // console.log(mainHeading);
 
 // document.getElementById("main-heading");
+const heading = document.getElementById("main-heading");
+
+heading.style.backgroundColor= "green"
+
+heading.style.padding = "15px"
+
+heading.style.borderRadius = "10px"
+
+heading.textcontent //'Manage Your Task Test'
+heading.innerText   //'Manage Your Task'
+heading.innerHTML   //'Manage Your Task <span style="display: none;">Test</span>'
 
 // document.getElementById("main-heading").innerHTML = "<h1> All Task </h1>"
 
-// document.getElementById("main-heading").className
+// document.getElementById("main-heading").className // give className
+
+document.getElementsByClassName("heading2")
 
 document.getElementById("main-heading").getAttribute("id") // get attribute
 document.getElementById("main-heading").setAttribute("class", "heading2") // setting the class/id attribute name.
