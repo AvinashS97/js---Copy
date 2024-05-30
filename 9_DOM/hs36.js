9a8f7acf9b18c947ee423a68d22b74dd37735f22
AvinashS97 <avikumar97170@gmail.com>
2024-05-30 17:42:04 +0530

    add Element to DOM

--- 9_DOM/hs36.js
+++ 9_DOM/hs36.js
@@ -9,14 +9,24 @@ const list=document.querySelectorAll("li")
 list[0].style.color = "green"
 
 list.forEach(function(l){
-    l.style.backgroundColor = "red"
+    l.style.backgroundColor = "grey"
 })
 
 // select element using className
 
 // document.getElementById("main-heading").className // give className
 document.getElementById("main-heading").getAttribute("id") // get attribute
-
 document.getElementById("main-heading").setAttribute("class", "heading2") // setting the class/id attribute name.
-
 document.getElementsByClassName("heading2")
+
+//How to apply loop on HTMLCollection
+const navList = document.getElementsByClassName("nav-list")
+const myArray= Array.from(navList)
+myArray // [li.nav-list, li.nav-list, li.nav-list]
+
+//applying loops
+myArray.forEach(function (li) {
+    li.style.color= "orange"
+})
+
+
