6fb9b5754de2109850ed7d87cc1a81e1a0e2316d
AvinashS97 <avikumar97170@gmail.com>
2024-05-30 13:23:43 +0530

    Dom

--- 9_DOM/hs35.js
+++ 9_DOM/hs35.js
@@ -7,49 +7,29 @@
 // console.log(window.document);
 // console.dir(document);
 
-//Finding HTML Objectss
-// console.log(document.links);// to get all <area> or <a> that have href attribute.
-// console.log(document.body); // return the <body> element.
-// console.log(document.URL); //returns the complete URL of the document
-
-
+// Finding HTML Objects
+console.log(document.links);// to get all <area> or <a> that have href attribute.
+console.log(document.body); // return the <body> element.
+console.log(document.URL); //returns the complete URL of the document
 
 // Select element using getElementById...
 
-// console.log(document.getElementById("main-heading")); // it return objects.
-// console.dir(document.getElementById("main-heading")); // it return objects.
-
-//select element using query selector
-// console.log(document.querySelector("#main-heading")); // it return objects.
-document.querySelector("h1"); //<h1 class=​"logo">​DOM Website​</h1>​
-document.querySelector("input[type=password]"); //document.querySelector("input[type="password"]")
-
-const list=document.querySelectorAll("li")
-
-list[0].style.color = "green"
-
-list.forEach(function(l){
-    l.style.backgroundColor = "red"
-})
+console.log(document.getElementById("main-heading")); // it return objects.
+console.dir(document.getElementById("main-heading")); // it return objects.
 
 //Change Text (DOM MANIPULATION)
-// const mainHeading = document.getElementById("main-heading");
-// console.log(mainHeading);
 
 // document.getElementById("main-heading");
 const heading = document.getElementById("main-heading");
-
 heading.style.backgroundColor= "green"
-
 heading.style.padding = "15px"
-
 heading.style.borderRadius = "10px"
 
 heading.textcontent //'Manage Your Task Test'
 heading.innerText   //'Manage Your Task'
 heading.innerHTML   //'Manage Your Task <span style="display: none;">Test</span>'
 
-// document.getElementById("main-heading").innerHTML = "<h1> All Task </h1>"
+document.getElementById("main-heading").innerHTML = "<h1> All Task </h1>"
 
 // document.getElementById("main-heading").className // give className
 
---
+++ 9_DOM/hs36.js
@@ -0,0 +1,22 @@
+//select element using query selector
+
+// console.log(document.querySelector("#main-heading")); // it return objects.
+document.querySelector("h1"); //<h1 class=​"logo">​DOM Website​</h1>​
+document.querySelector("input[type=password]"); //document.querySelector("input[type="password"]")
+
+const list=document.querySelectorAll("li")
+
+list[0].style.color = "green"
+
+list.forEach(function(l){
+    l.style.backgroundColor = "red"
+})
+
+// select element using className
+
+// document.getElementById("main-heading").className // give className
+document.getElementById("main-heading").getAttribute("id") // get attribute
+
+document.getElementById("main-heading").setAttribute("class", "heading2") // setting the class/id attribute name.
+
+document.getElementsByClassName("heading2")
