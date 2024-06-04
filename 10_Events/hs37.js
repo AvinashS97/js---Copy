8f3a1950d408eaa21321c8a7f5977476b38e55e5
AvinashS97 <avikumar97170@gmail.com>
2024-06-04 17:13:04 +0530

    .2

--- 10_Events/hs37.js
+++ 10_Events/hs37.js
@@ -2,3 +2,14 @@
 // For example:
 // The user selects, clicks, or hovers the cursor over a certain element.
 // The user chooses a key on the keyboard.
+
+const btn = document.querySelector("button");
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+btn.addEventListener("click", () => {
+  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+});
