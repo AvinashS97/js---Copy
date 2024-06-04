a1ff0f2c7557b656933704747b177dec8c2e6ae2
AvinashS97 <avikumar97170@gmail.com>
2024-06-04 16:48:01 +0530

    Events

---
+++ 10_Events/hs37.js
@@ -0,0 +1,4 @@
+// Events are things that happen in the system you are programming — the system produces (or "fires") a signal of some kind when an event occurs,
+// For example:
+// The user selects, clicks, or hovers the cursor over a certain element.
+// The user chooses a key on the keyboard.
