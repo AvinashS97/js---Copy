911f606cda46f7b8fab8bff2cbecbdf0717d27db
AvinashS97 <avikumar97170@gmail.com>
2024-06-12 12:37:47 +0530

    .1

--- 1_Basics_Of_JS/hs3.js
+++ 1_Basics_Of_JS/hs3.js
@@ -9,26 +9,26 @@ const student = true;
 let money = undefined;
 let pocket = null;
 
-console.log(typeof FName,typeof age,typeof marks_grade,typeof student,typeof money,typeof pocket);
+console.log(typeof FName, typeof age, typeof marks_grade, typeof student, typeof money, typeof pocket);
 
 // Data Conversion
 // String to number---> To change into Number just add "+" before the String.
-let x="Avinash"
-console.log(typeof(+"x"));
+let x = "Avinash"
+console.log(typeof (+"x"));
 // Or
-x=Number(x);
+x = Number(x);
 console.log(typeof x);
 // Number to string---> To change into String just add "" After the Number.
-let newAge= 21;
-console.log(typeof(21+ ""));
+let newAge = 21;
+console.log(typeof (21 + ""));
 // Or
-newAge= String(newAge);
+newAge = String(newAge);
 console.log(typeof newAge);
 
 // String Concatenation
-let string1= "Avinash";
-let string2= "Kumar";
-let fullName = string1 + "  " +  string2;
+let string1 = "Avinash";
+let string2 = "Kumar";
+let fullName = string1 + "  " + string2;
 console.log(fullName);
 
 // concatenation in additiion
@@ -40,7 +40,7 @@ console.log(Nage);
 // Template String
 let age1 = 21;
 let name1 = "Avinash";
-let aboutMe = "My name is "+name1+" and my age is "+age1+" ";
+let aboutMe = "My name is " + name1 + " and my age is " + age1 + " ";
 console.log(aboutMe);
 
 let aboutMe1 = `My name is ${name1} and my age is ${age1}. `
