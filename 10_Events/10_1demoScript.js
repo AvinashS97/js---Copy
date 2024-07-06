5b0829e11a36cfbc51d909c296bf8e8273e0f1a4
AvinashS97 <avikumar97170@gmail.com>
2024-07-06 21:27:50 +0530

    .3

--- 10_Events/10_1demoScript.js
+++ 10_Events/10_1demoScript.js
@@ -1,18 +1,18 @@
 const mainbutton = document.querySelector("button");
-const body= document.body;
+const body = document.body;
 const currentcolor = document.querySelector(".current-color")
 // console.log(currentcolor)
 
 function random() {
-    const red = Math.floor(Math.random() *256);
-    const green = Math.floor(Math.random() *256);
-    const blue = Math.floor(Math.random() *256);
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
     const rndCol = `rgb(${red}, ${green}, ${blue})`
     return rndCol
 }
 // math.random() help to generate  random number for colors...
-  
-mainbutton.addEventListener("click",()=>{
+
+mainbutton.addEventListener("click", () => {
     // console.log("button clicked")
     const rndCol = random();
     // console.log(rndCol)
