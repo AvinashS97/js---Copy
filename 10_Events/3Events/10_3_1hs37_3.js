74b0ccb09cd30694897e5424a276a210fe44bd7f
AvinashS97 <avikumar97170@gmail.com>
2024-08-05 16:28:32 +0530

    02-08-40

---
+++ 10_Events/3Events/10_3_1hs37_3.js
@@ -0,0 +1,32 @@
+// console.log("Hello")
+
+const todoForm = document.querySelector(".form-todo");
+const todoInput = document.querySelector(".form-todo input[type='text']");
+const todoList = document.querySelector(".todo_list");
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault(); //* It prevent to reload the page
+    // const newTodoText = console.log(todoInput.value); //* Print the input value
+    const newTodoText = todoInput.value; //* It will Store the input value 
+    const newLi = document.createElement("li");
+    const newLiInnerHtml = 
+        `<span class="text">${newTodoText}</span>
+        <div class="todo-buttons">
+            <button class="todo-btn done">Done</button>
+            <button class="todo-btn remove">Remove</button>
+        </div>`;
+    newLi.innerHTML = newLiInnerHtml;
+    todoList.append(newLi);
+    // console.log(newLi)
+    todoInput.value = "";
+});
+
+todoList.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("remove")){
+        console.log("You want to remove something ???")
+    };
+    if(e.target.classList.contains("done")){
+        const liSpan = e.parentNode.previousElementSibling;
+        console.log(liSpan);
+    };
+    
+})
