e444a8ce2411e4f66fd0e8e9be38d9a2314c72c5
AvinashS97 <avikumar97170@gmail.com>
2024-08-05 15:08:04 +0530

    bubbling, delegation

--- 10_Events/3Events/10_3hs37_3.js
+++ 10_Events/3Events/10_3hs37_3.js
@@ -10,30 +10,40 @@ const child = document.querySelector(".child");
 
 //** Event Bubling or Event Propagation */
 
-child.addEventListener("click", ()=>{
-    console.log("You Clicked Child")
-})
-parent.addEventListener("click", ()=>{
-    console.log("You Clicked Parent")
-})
-grandparent.addEventListener("click", ()=>{
-    console.log("You Clicked Grandparent")
-})
-document.body.addEventListener("click", ()=>{
-    console.log("You clicked Body")
-})
+//** Capturing Event */
 
-//** Capturing Event */ (=== 01:33:10 ===)
+// child.addEventListener("click", () => {
+//     console.log("Captured !.. Child")
+// }, false)
+// parent.addEventListener("click", () => {
+//     console.log("Captured !.. Parent")
+// }, true)
+// grandparent.addEventListener("click", () => {
+//     console.log("Captured !.. Grandparent")
+// }, true)
+// document.body.addEventListener("click", () => {
+//     console.log("Captured !.. Body")
+// }, true)
 
-child.addEventListener("click", () => {
-    console.log("Captured !!! Child")
-}, false)
-parent.addEventListener("click", () => {
-    console.log("Captured !!! Parent")
-}, false)
-grandparent.addEventListener("click", () => {
-    console.log("Captured !!! Grandparent")
-}, false)
-document.body.addEventListener("click", () => {
-    console.log("Captured !!! Body")
-}, false)
+//** Event Bubbling */ (=== 01:33:10 ===)
+
+// child.addEventListener("click", ()=>{
+//     console.log("You Clicked Child")
+// })
+// parent.addEventListener("click", ()=>{
+//     console.log("You Clicked Parent")
+// })
+// grandparent.addEventListener("click", ()=>{
+//     console.log("You Clicked Grandparent")
+// },false)
+// document.body.addEventListener("click", ()=>{
+//     console.log("You clicked Body")
+// })
+
+//** Event Delegation */
+
+grandparent.addEventListener("click",(e)=>{
+    console.log("Delegation on Grandparent")
+    console.log(e.target);
+    console.log(e.target.textContent);
+})
