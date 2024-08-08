f8cd45a81e2c959a2bb88958ce66bcb59d94785b
AvinashS97 <avikumar97170@gmail.com>
2024-08-08 16:19:24 +0530

    Async...

--- 11_Async/11hs_script.js
+++ 11_Async/11hs_script.js
@@ -1,2 +1,12 @@
-console.log("Hello")
+//? JavaScript is the Synchronous programming as it execute in the way we call i.e., in synchronous way and it is single threaded language... 
 
+function myFirst() {
+    console.log("Hello");
+  }
+  
+  function mySecond() {
+    console.log("Goodbye");
+  }
+  
+  myFirst();
+  mySecond();
