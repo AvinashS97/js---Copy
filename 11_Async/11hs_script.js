3a94323381fbfcda1bccd5f1b82294e28c1bf8fb
AvinashS97 <avikumar97170@gmail.com>
2024-08-11 18:31:31 +0530

    timeout

--- 11_Async/11hs_script.js
+++ 11_Async/11hs_script.js
@@ -1,12 +1,25 @@
 //? JavaScript is the Synchronous programming as it execute in the way we call i.e., in synchronous way and it is single threaded language... 
 
 function myFirst() {
-    console.log("Hello");
-  }
-  
-  function mySecond() {
-    console.log("Goodbye");
-  }
-  
-  myFirst();
-  mySecond();
+  console.log("Hello");
+}
+
+function mySecond() {
+  console.log("Goodbye");
+}
+
+myFirst();
+mySecond();
+console.log("Sync programming ---> it will execute after the above execution");
+
+//* The above is the example of sync programming...
+
+//? setTimeout function ---> It take 2 argument ===> a callback fns and a time after which the fns will execute...
+
+console.log("setTimeout start");
+
+setTimeout(()=>{
+  console.log("In setTimeout fns") 
+}, 1000);
+console.log("setTimeout Ends");
+
